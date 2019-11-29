cf41f93359e595208fd77bd54591a672c3bc0515
Genty Laurent <Laurent.Genty@bordeaux-inp.fr>
2019-11-29 12:02:43 +0100

    base files for connection in phpé

---
+++ connect.php
@@ -0,0 +1,6 @@
+<?php
+  $login = ''/*A compléter*/;
+  $db_pwd = ''/*A compléter*/;
+  /* Creation de l'objet qui gere la connexion: */
+  $connection = new mysqli("localhost", $login, $db_pwd, $login);
+?>
