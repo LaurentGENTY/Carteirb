d27ce09b162f97aa4c68335243a1af90375f94fc
Genty Laurent <Laurent.Genty@bordeaux-inp.fr>
2019-12-11 20:01:31 +0100

    laissez moi rentrer chez moi

--- src/frontend/AddPartiesToTournoi.php
+++ src/frontend/AddPartiesToTournoi.php
@@ -6,7 +6,7 @@ include "connect.php";
 if(isset($_GET["id"])) {
   $id_tournoi = $_GET["id"];
   AddParties($connection,$id_tournoi);
-  ShowDeck($connection,);
+  showPartiesTournoi($connection,$id_tournoi);
 }
 
 
