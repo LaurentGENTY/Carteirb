9b89b567efef927968d1f29f920e133ffdca98fd
Dylan Hertay <dhertay@enseirb-matmeca.fr>
2019-12-11 19:58:29 +0100

    Modify Tournoi

--- frontend/Exemplaires.php
+++ frontend/Exemplaires.php
@@ -4,13 +4,74 @@ include "Header.php";
 
 if(isset($_GET["id"])) {
   $id_joueur = $_GET["id"];
+  addExemplaire($connection,$id_joueur);
   showExemplaireJoueur($connection,$id_joueur);
 } else {
   header('Location: /Cards.php');
   exit();
 }
 
-
+function addExemplaire($connection,$id_joueur) {
+    ?>
+    <h1> Ajouter un Exemplaire </h1>;
+    <div class="row">
+      <form action="/AddExemplaire.php" class="col s12" method="post">
+        <div class="row">
+          <div class="input-field col s6">
+            <i class="material-icons prefix">title</i>
+            <input placeholder="Titre de la carte" id="Carte" type="text" class="validate" name="Carte">
+            <label class="active" for="Carte">Titre de la carte</label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s6">
+              <i class="material-icons prefix">merge_type</i>
+              <input  placeholder="Edition" id="Edition" type="text" class="validate" name="Edition">
+              <label class="active" for="Edition">Edition</label>
+            </div>
+          </div>
+        <div class="row">
+          <div class="input-field col s6">
+            <i class="material-icons prefix">whatshot</i>
+            <input  placeholder="Qualite" id="Quality" type="number" class="validate" name="Quality">
+            <label class="active" for="Quality">Qualité de la carte</label>
+          </div>
+          </div>
+          <div class="row">
+            <div class="input-field col s6">
+              <i class="material-icons prefix">whatshot</i>
+              <input  placeholder="Effet impression" id="ImpressionEffect" type="text" class="validate" name="ImpressionEffect">
+              <label class="active" for="ImpressionEffect">Effet d'impression</label>
+            </div>
+            </div>
+        <div class="row">
+          <div class="input-field col s6">
+              <i class="material-icons prefix">apps</i>
+              <input  placeholder="Date acquisition" id="DateAcq" type="date" class="validate" name="DateAcq">
+              <label class="active" for="DateAcq">Date d'acquisition</label>
+          </div>
+        </div>
+          <div class="row">
+            <div class="input-field col s6">
+                <i class="material-icons prefix">insert_link</i>
+                <input  placeholder="Mode acquisition" id="ModeAcq" type="text" class="validate" name="ModeAcq">
+                <label class="active" for="ModeAcq">Mode d'acquisition</label>
+              </div>
+            </div>
+            <div class="row">
+              <div class="input-field col s6">
+                  <i class="material-icons prefix">apps</i>
+                  <input  placeholder="Date de perte" id="DateLos" type="date" class="validate" name="DateLos">
+                  <label class="active" for="DateLos">Date de perte </label>
+              </div>
+            </div>
+      <button class="btn waves-effect waves-light" type="submit">Ajouter un exemplaire
+        <i class="material-icons right">add</i>
+      </button>
+      </form>
+    </div>
+    <?php
+}
 
 /* Afficher les cartes dun joueur */
 function showExemplaireJoueur($connection,$id_joueur) {
